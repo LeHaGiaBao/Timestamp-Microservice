6301e06671656cf9452b6e0b815d73d4dc36176f
Le Ha Gia Bao <80085425+LeHaGiaBao@users.noreply.github.com>
2022-05-19 23:06:51 +0700

    Change index.js

--- index.js
+++ index.js
@@ -1,37 +1,77 @@
-const express = require('express');
-const app = express();
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+const port = process.env.PORT || 8000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-const cors = require('cors');
-app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// Root endpoint. Display index file
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
-
-// Handle returning a timestamp
-app.get('/api/timestamp/:date?', (req, res) => {
-    // Store our date response. This will default to the current datetime
-    let date = new Date();
-
-    // Check if the optional date parameter was provided
-    if (req.params.date) {
-        // Convert the date parameter to a string
-        let unixDate = +req.params.date;
-
-        // Check if the date passed is unix time. If it's not, use the date string provided
-        date = isNaN(unixDate) ? new Date(req.params.date) : new Date(unixDate);
-
-        // Check if the date created is valid. Throw an error if it's an invalid date
-        if (!(date instanceof Date) || isNaN(date.getTime())) return res.json({ error: "Invalid Date" });
-    }
-
-    // Return the unix and UTC time
-    return res.json({ unix: date.getTime(), utc: date.toUTCString() });
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// Create a listener to handle requests
-const listener = app.listen(process.env.PORT, () => console.log('Your app is listening on port ' + listener.address().port));
+app.get('/api', (req,res) =>{
+  let date = new Date();
+  
+  let result = {
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  }
+
+  res.send(result);
+});
+
+app.get('/api/:date',(req,res) => {
+
+  //Handling data parameters with invalid format
+
+  if(!Date.parse(req.params.date) && !Number(req.params.date))
+  {
+    return res.send({error: "Invalid Date"});
+  }
+
+
+  //Checking for conditions when date parameter is given in microseconds.
+
+  else if(!(/[-]/.test(req.params.date)) && Number(req.params.date))
+  {
+    let date = new Date(Number(req.params.date));
+
+    return res.send({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  } 
+
+  //For handling regular test cases when date parameter is in a valid date format.
+
+  let date = new Date(req.params.date);
+
+  let result = {
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  }
+
+  res.status(200).send(result);
+});
+
+// your first API endpoint... 
+// app.get("/api/hello", function (req, res) {
+//   res.json({greeting: 'hello API'});
+// });
+
+
+
+// listen for requests :)
+var listener = app.listen(port, function () {
+  console.log('Your app is listening on port ' + port);
+});
