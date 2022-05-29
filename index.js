f29206bea77e2a827200935558cff0faf2ecb262
Le Ha Gia Bao <80085425+LeHaGiaBao@users.noreply.github.com>
2022-05-29 20:03:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-// server.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 8000;
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -32,15 +24,10 @@ app.get('/api', (req,res) =>{
 
 app.get('/api/:date',(req,res) => {
 
-  //Handling data parameters with invalid format
-
   if(!Date.parse(req.params.date) && !Number(req.params.date))
   {
     return res.send({error: "Invalid Date"});
   }
-
-
-  //Checking for conditions when date parameter is given in microseconds.
 
   else if(!(/[-]/.test(req.params.date)) && Number(req.params.date))
   {
@@ -52,8 +39,6 @@ app.get('/api/:date',(req,res) => {
     });
   } 
 
-  //For handling regular test cases when date parameter is in a valid date format.
-
   let date = new Date(req.params.date);
 
   let result = {
@@ -64,14 +49,6 @@ app.get('/api/:date',(req,res) => {
   res.status(200).send(result);
 });
 
-// your first API endpoint... 
-// app.get("/api/hello", function (req, res) {
-//   res.json({greeting: 'hello API'});
-// });
-
-
-
-// listen for requests :)
-var listener = app.listen(port, function () {
+const listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + port);
 });
